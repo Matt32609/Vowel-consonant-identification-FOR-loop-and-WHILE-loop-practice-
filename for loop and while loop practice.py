f3137b863a876146aa7eb4ebc07d27c4c42a062b
Matt32609 <vthematt@gmail.com>
2026-02-13 14:22:56 +0800

    Add files via upload

--- for loop and while loop practice.py
+++ for loop and while loop practice.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python3
 
 letter = "abcdefghijklmnopqrstuvwxyz"
-lemma = str(input("Please enter a letter :"))
+lemma = str(input("Please enter a letter :")).lower()
 
 def lemman():
     for x in lemma:
         if x in "aeiou":
             print("This is a vowel.")
-            gg = input("Do you want to try again? Please say 'yes' or 'no' to continue.")
+            gg = input("Do you want to try again? Please say 'yes' or 'no' to continue.").lower()
             if gg == "yes":
                 return True
             else:
                 exit()
         elif x in letter:
             print("This is a consonant.")
-            gg = input("Do you want to try again? Please say 'yes' or 'no' to continue.")
+            gg = input("Do you want to try again? Please say 'yes' or 'no' to continue.").lower()
             if gg == "yes":
                 return True
             else:
@@ -27,7 +27,7 @@ def lemman():
 lemman()
 
 while True:
-    lemma = str(input("Please enter a letter :"))
+    lemma = str(input("Please enter a letter :")).lower()
     lemman()
 
 
